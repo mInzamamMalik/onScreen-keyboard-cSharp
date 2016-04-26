a3ab68c5653916e788bbd85248b87a0a03fa42eb
malikasinger1@gmail.com <malikasinger1@gmail.com>
2016-04-26 20:47:15 +0500

    simple key press implemented :-)

--- onScreen-keyboard cSharp/Form1.cs
+++ onScreen-keyboard cSharp/Form1.cs
@@ -12,9 +12,26 @@ namespace onScreen_keyboard_cSharp
 {
     public partial class Form1 : Form
     {
+        const int WS_EX_NOACTIVATE = 0x08000000;
+
         public Form1()
         {
             InitializeComponent();
+            
+        }
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams param = base.CreateParams;
+                param.ExStyle |= WS_EX_NOACTIVATE;
+                return param;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.SendKeys.SendWait("G");
         }
     }
 }
